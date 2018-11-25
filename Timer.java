06e2f06d21cc26df06ce05550883721cba9c0340
ASUS ROG\ryanb <ryanb.atk@gmail.com>
2018-11-24 18:42:29 -0600

    Timer is nearly complete. The constructor will take objects of the system managers so that it can call methods in them. I can't remember what storeTransaction() is supposed to do.

--- Timer.java
+++ Timer.java
@@ -1,30 +1,54 @@
 import java.util.ArrayList;
-import java.time.LocalTime;
+import java.time.*;
 
 public class Timer {
 	
 	LocalTime time;
+	Check-outManager cm;
+	StockManager sm;
 
-    public Timer() {
-    	
+    public Timer(Check-outManager c, StockManager m) {
+    	time = LocalTime.now("CST");
+    	cm = c;
+    	sm = s;
     }
     
     public void getTime() {
-		LocalDateTime.now();
+		time = LocalTime.now("CST");
 	}
 	
-	public void generateDailyReport() {
-		if(time == )
+	public boolean checkMidnight()
+	{
+		getTime();
+		int hour = time.getHour();
+		int minute;
+		if (hour == 0)
+		{
+			minute = time.getMinute();
+				if (minute <= 10) //if it is within 5 minutes of midnight
+				return true;
+		}
+		else
+			return false;
+		return false;
+	}
+	
+	public void timedEvents() {
+		if (checkMidnight())
+		{
+			ArrayList<Item> allSold = cm.generateDailyReport();
+			sm.updateStockFromReport(allSold);
+			printInventoryReport();
+		}
 		
 	}
 	
-	public void storeTransaction() {
-		
-	}
+	/*public void storeTransaction() { //might not need
+		if (checkMidnight())
+			cm.generateDailyReport();
+	} */
 	
 	public void printInvertoryReport() { 
-		
-	}
-    
-    
+		sm.generateInventoryReport();
+	} 
 }
