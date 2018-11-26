a58881204bc33b2149e7b32a278ff057c97f527d
ASUS ROG\ryanb <ryanb.atk@gmail.com>
2018-11-25 22:46:12 -0600

    Revert "updated gitignore and renamed Check-outManager to CheckoutManger because java classes can't have hypens in them"
    This reverts commit e1400b9a40a9eeea36952a3df0aeaa0c0e55d639.

---
+++ Check-outManager.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.awt.event.KeyEvent;
+import java.lang.Scanner;
+
+public class Check-outManager
+{
+	ArrayList<Transaction> transactions;
+	Customer customer;
+	Cashier cashier;
+
+    public Check-outManager() 
+    {
+    	
+    }
+    
+    
+}
